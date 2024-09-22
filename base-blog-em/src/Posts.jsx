7fabc90bbc84c120b74b4d2df47e79c71ab9cd73
CGuillotG <carlos@guillot.dev>
2024-09-21 22:38:11 -0600

    Added stale time to post fetching

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -11,9 +11,12 @@ export function Posts() {
 
   const { data, isLoading, isError, error } = useQuery({
     queryKey: ["posts"],
-    queryFn: fetchPosts
+    queryFn: fetchPosts,
+    staleTime: 2000, // 2 seconds
   });
+
   if (isLoading) return <h3>Loading...</h3>;
+
   if (isError) return (
     <>
       <h3>Error!</h3>
